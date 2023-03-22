cb061ac3fa65e2c4f00463ba8e64c08ca57492ff
Sdrs81 <serdardemirtas25@gmail.com>
2023-03-22 18:42:00 +0300

    Datas are being stored local database (json-server) now, so datas will remain after restarting the page.

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ The technologies and methods that used in this project are as follows:
 - Math Object, Arrow Functions, Map usage and Array methods
 - Ternary operator usage to display different views
 - HTTP Client called 'Axios'
+- Async Structure
 - NPM Package called 'json-server'
 
-Note: After cloning this project, you should run 'npm install' command in terminal to make project ready to work.
+Note: After cloning this project, you should run 'npm install' command in terminal to make project ready to work. After that, you need to start json-server from terminal for full functionality.
--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
 import './App.css'
 import TaskCreate from './components/TaskCreate'
 import TaskList from './components/TaskList'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
 
   const [tasks, setTasks] = useState([])
 
-  const createTask = (title, taskDesc) =>{
+  const createTask = async (title, taskDesc) =>{
+
+    const response = await axios.post('http://localhost:3000/tasks', {
+      title: title,
+      taskDesc // if key and value is equal name like above line, we can use it like this
+    });
+
     const createdTasks = [
-      ...tasks,{
-        id: Math.round(Math.random()*999999),
-        title: title,
-        taskDesc,   // if key and value is equal name like above line, we can use it like this
-      }
+      ...tasks,
+      response.data
     ];
     setTasks(createdTasks);
   }
 
-  const deleteTaskById = (id) =>{
+  const fetchTasks = async () =>{
+    const response = await axios.get('http://localhost:3000/tasks');
+    setTasks(response.data);
+  } 
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);   // just works when project restarted
+
+  const deleteTaskById = async (id) =>{
+    await axios.delete(`http://localhost:3000/tasks/${id}`);  // if we pass data to url, we need this `` instead of ''
     const afterDeletingTasks = tasks.filter((task) => {
       return task.id !== id;
     })
     setTasks(afterDeletingTasks);
   }
 
-  const editTaskById = (id, updatedTitle, updatedTaskDesc) =>{
+  const editTaskById = async (id, updatedTitle, updatedTaskDesc) =>{
+    await axios.put(`http://localhost:3000/tasks/${id}` ,{
+      title: updatedTitle,
+      taskDesc: updatedTaskDesc
+    }); 
     const afterUpdatingTasks = tasks.map((task) => {
       if(task.id === id){
       return { id, title: updatedTitle, taskDesc: updatedTaskDesc}
